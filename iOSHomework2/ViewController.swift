75baef0a276562cf7d88d1966758b59afcd29a6f
Omar <omar@barmej.com>
2020-06-16 08:53:27 +0300

    Fixing variables names

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -9,80 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var membersNamesArray  : [String] = [""]
+    
+    var convertToLetter = true
+    @IBOutlet weak var secretSocietyNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBAction func addMember(_ sender: Any) {
+        
+        let member = nameTextField.text!
+        
+        // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
+        
+        
+        
+        // MARK: -   النهاية
+        
+        nameTextField.text = ""
     }
     
-    var membernamearray  : [String] = [""]
     
-    var ConvertToletter = true
-    @IBOutlet weak var secretsocietynamelabel: UILabel!
-    @IBOutlet weak var nametextfield: UITextField!
-    @IBAction func Addmember(_ sender: Any) {
-        
-        let member = nametextfield.text!
-        
-// MARK: -   1️⃣ تحت الخط memberNameArray إلي المصفوفه memberقم بإضافة المتغير
-
-        
-
-// MARK: -   النهاية
-   
-        nametextfield.text = ""
-    }
     
-
-        
     
-    @IBAction func Letterbutton(_ sender: Any) {
-      
-// MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
+    @IBAction func letterButton(_ sender: Any) {
+        
+        // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
         var functionCall = ""
         
         
-// MARK: -   النهاية
-
-            
-             secretsocietynamelabel.text =  functionCall
-    }
-    
-    
-    
-    
-    @IBAction func emojibutton(_ sender: Any) {
-      
-        
-        
-// MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
-                
-                var functionCall = ""
-                
-// MARK: -   النهاية
-       
-        secretsocietynamelabel.text =  functionCall
-        
-       
-    }
-    
-    
-    
-    
- // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
-    
-
         // MARK: -   النهاية
-
+        
+        
+        secretSocietyNameLabel.text =  functionCall
+    }
+    
+    
+    
+    
+    @IBAction func emojiButton(_ sender: Any) {
+        
+        
+        
+        // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
+        
+        var functionCall = ""
+        
+        // MARK: -   النهاية
+        
+        secretSocietyNameLabel.text =  functionCall
+        
+        
+    }
+    
+    
+    
+    
+    // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
+    
+    
+    // MARK: -   النهاية
+    
     
     
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
-          
     
-          // MARK: -   النهاية
-
+    
+    // MARK: -   النهاية
+    
     
     
 }
